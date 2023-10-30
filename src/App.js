25088dc3545c1ef4fa980b6cc2366ba12624085f
OhHyukJun <kmh7277@naver.com>
2023-10-30 17:29:03 +0900

    Feat: 라우팅 설정 및 코드 import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
+import Calendar from 'react-calendar';
+import { DatePick } from './Pages/Calendar/DatePick';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { DateInput } from './Pages/Calendar/DateInput';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Calendar/>}/>
+          <Route path="/dateInput" element={<DateInput />}/>
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
+    </>
   );
 }
 
