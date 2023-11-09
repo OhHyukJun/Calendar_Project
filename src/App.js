fe6caceb1fc347fc42fbfa59c85c4b1f05ca7983
OhHyukJun <kmh7277@naver.com>
2023-11-09 15:34:42 +0900

    Feat: 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import {
   useRecoilState,
   useRecoilValue,
 } from "recoil";
-import Calendar from 'react-calendar';
-import { DatePick } from './Pages/Calendar/DatePick';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DateInput } from './Pages/Calendar/DateInput';
+import Header from './layouts/Header';
+
 function App() {
   return (
     <>
     <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Calendar/>}/>
-          <Route path="/dateInput" element={<DateInput />}/>
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<DateInput />}/>
+          </Routes>
+        </BrowserRouter>
     </RecoilRoot>
     </>
   );
