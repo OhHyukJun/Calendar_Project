375c28de67392f2b13bd3ae3d4cb21806e0f9da2
OhHyukJun <kmh7277@naver.com>
2023-11-09 15:32:23 +0900

    Refactor: 라이브러리 교체

--- src/Pages/Calendar/DateInput.js
+++ src/Pages/Calendar/DateInput.js
@@ -1,8 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import { DatePick } from "./DatePick";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from 'moment';
+//import 'react-calendar/dist/Calendar.css';
+import styled from "styled-components";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const TextContainer = styled.span`
+  display:flex;
+  justify-content: center;
+  margin-top:1vw;
+`;
+
+
 
 export const DateInput = () => {
+  moment.locale('ko-KR');
+  const localize = momentLocalizer(moment);
   const [date, setDate] = useState();
 
   useEffect(() => {
@@ -19,16 +34,17 @@ export const DateInput = () => {
 
   return (
     <Container>
-      <form
-        onSubmit={handleSubmit}
-      >
+      <form onSubmit={handleSubmit}>
         <div>
-          <span>등록</span>
+          <Calendar
+            localizer={localize}
+            style={{ height: 800 }}
+          />
         </div>
         <div>
-          <div>
+          <TextContainer>
             <DatePick />
-          </div>
+          </TextContainer>
         </div>
         <Button type="submit">
           <span>등록하기</span>
