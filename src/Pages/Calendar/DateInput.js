5e1a716e22b79394c3f14ae517538ad8c8358f07
OhHyukJun <kmh7277@naver.com>
2023-11-16 16:46:09 +0900

    Feat: 달려 코드 작성 중

--- src/Pages/Calendar/DateInput.js
+++ src/Pages/Calendar/DateInput.js
@@ -2,54 +2,127 @@ import React, { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import { DatePick } from "./DatePick";
 import { Calendar, momentLocalizer } from "react-big-calendar";
+import ReactModal from "react-modal";
 import moment from 'moment';
-//import 'react-calendar/dist/Calendar.css';
 import styled from "styled-components";
+import DatePicker from "react-datepicker";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import "react-datepicker/dist/react-datepicker.css";
 
-const TextContainer = styled.span`
-  display:flex;
-  justify-content: center;
-  margin-top:1vw;
+export const StyledModal = styled(ReactModal)`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #f5f5f5;
+  border-radius: 5%;
+  width:20vw;
+  height:10vw;
+  outline: none;
+  opacity:1;
+  display:flex-column;
+  h2 {
+    margin-top: 0;
+    color: #333;
+    font-size: 1.2rem;
+  }
+  .react-datepicker-wrapper {
+    display: inline-block;
+    margin-right: 0.1vw;
+  }
+  button{
+    margin:0.5vw;
+    width:4vw;
+  }
+  z-index: 10000;
 `;
 
-
+const myEventsList = [
+  {
+    id: 0,
+    title: '테스트 1',
+    allDay: true,
+    start: new Date("2023-11-13"),
+    end: new Date("2023-11-18"),
+  },
+  {
+    id: 1,
+    title: '테스트 2',
+    allDay: true,
+    start: new Date("2023-11-13"),
+    end: new Date("2023-11-17"),
+  }
+]
 
 export const DateInput = () => {
-  moment.locale('ko-KR');
-  const localize = momentLocalizer(moment);
-  const [date, setDate] = useState();
+    moment.locale('ko-KR');
+    const localize = momentLocalizer(moment);
+    const [events, setEvents] = useState(myEventsList);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null)
+    const [title, setTitle] = useState("");
 
-  useEffect(() => {
-    setDate({
-      ...date,
-    });
-  }, []);
+    const handleSelect = ({ start, end }) => {
+      setSelectedEvent({ start, end, title: "" });
+      setStartDate(new Date(start));
+      setEndDate(new Date(end));
+      setTitle("");
+      setModalIsOpen(true);
+    }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log("submit");
-    console.log(date);
-  }
+    const handleEventSelect = event => {
+      setSelectedEvent(event);
+      setStartDate(new Date(event.start));
+      setEndDate(new Date(event.end));
+      setModalIsOpen(true);
+    }
 
-  return (
-    <Container>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Calendar
-            localizer={localize}
-            style={{ height: 800 }}
-          />
-        </div>
-        <div>
-          <TextContainer>
-            <DatePick />
-          </TextContainer>
-        </div>
-        <Button type="submit">
-          <span>등록하기</span>
-        </Button>
-      </form>
-    </Container>
-  );
+    const handleSave = () => {
+      if (selectedEvent.id != null) {
+        setEvents(events.map(e => e.id === selectedEvent.id ? { ...e, start: startDate, end: endDate, title: title } : e));
+      } else {
+        setEvents([...events, { id: events.length, start: startDate, end: endDate, title: title }]);
+      }
+      setModalIsOpen(false);
+    }
+
+    const handleDelete = () => {
+      setEvents(events.filter(e => e.id !== selectedEvent.id));
+      setModalIsOpen(false);
+    }
+
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      console.log("submit");
+    }
+
+    return (
+      <Container>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Calendar
+              localizer={localize}
+              events={events}
+              startAccessor="start"
+              endAccessor="end"
+              style={{ height: 800 }}
+              onSelectSlot={handleSelect}
+              onSelectEvent={handleEventSelect}
+              selectable={true}
+            />
+          </div>
+            <StyledModal isOpen={modalIsOpen}>
+              <h2>{selectedEvent?.title}</h2>
+              <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+              <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
+              <Button onClick={handleSave}>저장</Button>
+              <Button onClick={handleDelete}>삭제</Button>
+              <Button onClick={() => setModalIsOpen(false)}>취소</Button>
+            </StyledModal>
+        </form>
+      </Container>
+    );
 };
