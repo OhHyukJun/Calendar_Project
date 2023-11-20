e2ca7131417f58805c331c9734191a41d4abe13a
OhHyukJun <kmh7277@naver.com>
2023-11-20 16:55:07 +0900

    Feat: 데이터베이스 연결 및 api 작성

--- server/server.js
+++ server/server.js
@@ -1,22 +1,42 @@
-const express = require('express');
+import express from "express";
+import pkg from 'mssql';
+import bodyParser from "body-parser";
+const { ConnectionPool } = pkg;
+
 
 const app = express();
-app.listen(4000, function () {
-    console.log('listening on 4000')
-});
+app.use(bodyParser.json());
 
-let mssql = require("mssql");
-let dbConfig_user = {
-    server: "10.1.2.92",
-    database: "callendar.database.windows.net",
-    user: "dhgurwns",
-    password: "kmh7277!",
-    port: 4000
+const dbConfig = {
+  server: "callendar.database.windows.net",
+  database: "Callendar",
+  user: "dhgurwns@callendar",
+  password: "kmh7277!",
+  port: 1433,
+  options: {
+    encrypt: true,
+    trustServerCertificate: false,
+  }
 };
 
-mssql.connect(dbConfig_user, function(err){
-    if(err){
-        return console.error('error : ', err);
-    }
-    console.log('MSSQL 연결 완료')
+const pool = new ConnectionPool(dbConfig);
+pool.connect().then(() => {
+  console.log('MSSQL 연결 완료');
+}).catch(err => console.log('MSSQL 연결 오류', err));
+
+app.post('/event', function (req, res) {
+    const event = req.body;
+    const sqlQuery = "INSERT INTO Events (title, start, end) OUTPUT INSERTED.id VALUES (?, ?, ?)";
+    const params = [event.title, event.start, event.end];
+
+    pool.request().query(sqlQuery, params).then(result => {
+      res.status(200).json({ id: result.recordset[0].id });
+    }).catch(err => {
+      console.error('error : ', err);
+      res.status(500).send('Error while saving event');
+    });
+});
+
+app.listen(4000, function () {
+    console.log('listening on 4000');
 });
